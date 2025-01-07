870e1fcb0c46b96ebb107a1924cbae181ff42c00
John Skinner <john@sknnr.io>
2025-01-07 09:11:59 -0600

    refactor backup script to use genbackup command

--- container/backup.sh
+++ container/backup.sh
@@ -1,21 +1,31 @@
 #!/bin/bash
-# Backup datestamp variable
-DATESTAMP=$(date +"%Y%m%d-%H%M%S%3N")
+DATESTAMP=$(date +"%Y-%m-%d-%H-%M-%S")
+VS_PID=$(supervisorctl pid vintagestory)
 
-# Stop Vintage Story
-echo "Stopping Vintage Story server process to perform backup"
-supervisorctl stop vintagestory
-sleep 15
+# Issue backup command to stdin of VS pid
+echo "/genbackup backup_${DATESTAMP}.vcdbs" > /proc/${vs_pid}/fd/0
 
-# Perform backup
-tar --exclude='./Backups' --exclude='./BackupSaves' -czf ${VINTAGE_STORY_PATH}/data/Backups/server_backup_${DATESTAMP}.tar.gz -C ${VINTAGE_STORY_PATH}/data .
-echo "Backup created at ${VINTAGE_STORY_PATH}/data/Backups/server_backup_${DATESTAMP}.tar.gz"
+# Wait for our file to exist
+while [ ! -f "${VINTAGE_STORY_PATH}/data/Backups/backup_${DATESTAMP}.vcdbs" ]; do
+    echo "Waiting for backup to be created"
+    sleep 2
+done
 
-# Cleanup old backups
+# Make sure writing of file is complete
+while lsof "${VINTAGE_STORY_PATH}/data/Backups/backup_${DATESTAMP}.vcdbs" > /dev/null; do
+    echo "Waiting for backup to be finalized"
+    sleep 2
+done
+
+# Compress the backup to save some space
+echo "Compressing backup"
+tar czf ${VINTAGE_STORY_PATH}/data/Backups/server_backup_${DATESTAMP}.tar.gz ${VINTAGE_STORY_PATH}/data/Backups/backup_${DATESTAMP}.vcdbs
+
+echo "Backup server_backup_${DATESTAMP}.tar.gz created"
+
+# Clean up original
+rm -f ${VINTAGE_STORY_PATH}/data/Backups/backup_${DATESTAMP}.vcdbs
+
+# Remove older backups
 echo "Cleaning up previous backups older than ${BACKUP_RETENTION_DAYS} days"
 find ${VINTAGE_STORY_PATH}/data/Backups -name "server_backup_*.tar.gz" -type f -mtime +$BACKUP_RETENTION_DAYS -exec rm -f {} \;
-
-# Start Vintage Story
-echo "Backup process complete, starting Vintage Story server process"
-sleep 15
-supervisorctl start vintagestory
